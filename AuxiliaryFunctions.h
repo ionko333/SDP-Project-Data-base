70bb29efd2b269d4c165e9d6ce028e87cec9393b
ionko333 <42829292+ionko333@users.noreply.github.com>
2019-12-26 16:40:58 +0200

    Final final optimizations
    Made Record into a class and added get and set functions and changed every function that uses the class to use them instead of accessing them by operator .

---
+++ AuxiliaryFunctions.h
@@ -0,0 +1,40 @@
+#ifndef AUXILIARYFUNCTIONS_H_INCLUDED
+#define AUXILIARYFUNCTIONS_H_INCLUDED
+#include "Record.h"
+#include <fstream>
+
+long long int getRecordsCnt()
+{
+	ifstream iFile;
+	if (!iFile.good())
+	{
+		return -1;
+	}
+	iFile.open("Planes.db", ios::binary);
+	iFile.seekg(0, ios::end);
+	int cnt = iFile.tellg() / sizeof(Record);
+	iFile.close();
+	return cnt;
+}
+
+int recordPosition(unsigned long long int Id)
+{
+	int recordsCnt = getRecordsCnt();
+	ifstream iFile;
+	iFile.open("Planes.db", ios::binary);
+	int pos = 0;
+	Record rec;
+	for (int i = 0; i < recordsCnt; ++i)
+	{
+		iFile.read((char*)&rec, sizeof(Record));
+		if (Id == rec.getId())
+		{
+			return pos;
+		}
+		pos++;
+	}
+	iFile.close();
+	return -1;
+}
+
+#endif // AUXILIARYFUNCTIONS_H_INCLUDED
---
+++ Update.h
@@ -0,0 +1,89 @@
+#ifndef UPDATE_H_INCLUDED
+#define UPDATE_H_INCLUDED
+#include "AuxiliaryFunctions.h"
+
+
+#include "AuxiliaryFunctions.h"
+
+
+void updateAttribute(bool isOptimized)
+{
+	long long int numOfRecords = getRecordsCnt();
+	if (numOfRecords == 0)
+	{
+		cout << "Data base is empty\n";
+		return;
+	}
+	cout << "Please type the Id of the record you want to update\n";
+	unsigned long long int Id;
+	cin >> Id;
+	cin.ignore();
+	int pos = recordPosition(Id);
+	if (pos == -1)
+	{
+		cout << "A record with this Id does not exist in the data base\n";
+		return;
+	}
+	string attribute;
+	cout << "Please type the attribute you want to change\n";
+	cin >> attribute;
+	cin.ignore();
+	fstream file;
+	file.open("Planes.db", ios::in | ios::out | ios::binary);
+	if (!file.good())
+	{
+		cerr << "File cannot be opened\n";
+		return;
+	}
+	file.seekg(pos * sizeof(Record), ios::beg);
+	Record rec;
+	file.read((char*)&rec, sizeof(Record));
+	if (attribute == "Id")
+	{
+		cout << "Please type the new Id for this record\n";
+		unsigned long long int newId;
+		cin >> newId;
+		cin.ignore();
+		bool alreadyExists = false;
+		int newPos = recordPosition(newId);
+		if (newPos > -1)
+		{
+			cout << "A record with the desired Id already exists in our data base \n";
+			return;
+		}
+		rec.setId(newId);
+		isOptimized = false;
+	}
+	else if (attribute == "Plane")
+	{
+		cout << "Please type the new Plane for this record\n";
+		char newPlane[257];
+		cin.getline(newPlane, 257);
+		rec.setPlane(newPlane);
+	}
+	else if (attribute == "Type")
+	{
+		cout << "Please type the new Type for this record\n";
+		char newType[257];
+		cin.getline(newType, 257);
+		rec.setType(newType);
+	}
+	else if (attribute == "Flights")
+	{
+		cout << "Please type the new number of flights for this record\n";
+		unsigned long long int newFlights;
+		cin >> newFlights;
+		cin.ignore();
+		rec.setFlights(newFlights);
+	}
+	file.seekp(pos * sizeof(Record), ios::beg);
+	if (!file.good())
+	{
+		cerr << "File is not good xD \n";
+		return;
+	}
+	file.write((const char*)&rec, sizeof(Record));
+	cout << "Update was successful \n";
+	file.close();
+}
+#endif // UPDATE_H_INCLUDED
